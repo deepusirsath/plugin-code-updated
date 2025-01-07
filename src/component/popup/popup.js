bbbc554f3bff4964f95bde445bbddccd08e136f2
deepali <deepali@ekvayu.com>
2025-01-07 17:41:25 +0530

    added documentatin in popup.js

--- src/component/popup/popup.js
+++ src/component/popup/popup.js
@@ -4,6 +4,28 @@ import {
 } from "/src/helper/content_loader_helper.js";
 import { COMPONENTS } from "/src/constant/constant.js";
 
+/**
+ * Dynamically loads a component by fetching its HTML content and JavaScript file
+ *
+ * @param {string} componentName - The name of the component to load (e.g. 'header', 'sidebar')
+ * @returns {Promise<void>} A promise that resolves when the component is loaded
+ *
+ * @example
+ * // Load the header component
+ * await loadComponent('header');
+ *
+ * @description
+ * The function performs the following steps:
+ * 1. Constructs the file path using the component name
+ * 2. Loads the HTML content from componentName.html
+ * 3. Injects the HTML into the container with id "{componentName}-container"
+ * 4. Loads and executes the associated JavaScript file
+ *
+ * @requires
+ * - loadHTMLContent from content_loader_helper.js
+ * - loadScript from content_loader_helper.js
+ * - A container element with id "{componentName}-container" must exist in the DOM
+ */
 const loadComponent = async (componentName) => {
   const fileName = `/src/component/${componentName}/${componentName}`;
   const htmlContent = await loadHTMLContent(`${fileName}.html`);
