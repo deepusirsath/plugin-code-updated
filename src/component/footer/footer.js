bc6ede2f2c30250cbb4dc1eb377bf3db40c7991c
deepali <deepali@ekvayu.com>
2025-01-08 12:03:11 +0530

    done the documenation in footer file

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,5 +1,8 @@
 /**
  * Executes the copyright year update by setting the current year in the DOM.
+ * @description Updates the element with id="currentYear" with the current year
+ * @example
+ * // Updates <span id="currentYear">2024</span> automatically
  */
 const showCurrentYear = () => {
   document.getElementById("currentYear").textContent = new Date().getFullYear();
