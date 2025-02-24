4b6e3864c5ee0260ecc264cbd8bbb101f79237fb
deepali <deepali.sirsath1998@gmail.com>
2025-02-24 12:23:18 +0530

    added changes in url

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // dev - http://192.168.0.2:10101/plugin
 // uat - http://192.168.0.2:10103/plugin
 // aws - http://43.204.32.124:8001/plugin
-// beta -http://192.168.0.2:5050/plugin
+// beta - http://192.168.0.2:5050/plugin
 
 const config = {
   BASE_URL: "http://192.168.0.2:5050/plugin",
