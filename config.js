5f94539241d56b5d5684335a704726004c3f3deb
deepali <deepali.sirsath1998@gmail.com>
2025-03-03 14:32:47 +0530

    added changes in url

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 // beta - http://192.168.0.2:5050/plugin
 
 const config = {
-  BASE_URL: "http://43.204.32.124:8001//plugin",
+  BASE_URL: "http://43.204.32.124:8001/plugin",
 };
 
 export default config;
