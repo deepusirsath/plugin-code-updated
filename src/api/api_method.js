5c0d6d4964ae43e54ed6c332969098079b29136e
deepali <deepali.sirsath1998@gmail.com>
2025-03-27 16:23:26 +0530

    added changes

--- src/api/api_method.js
+++ src/api/api_method.js
@@ -1,3 +1,7 @@
+import { BASEPATH } from "/src/constant/basepath.js";
+import { COMPONENTS } from "/src/constant/component.js";
+import { TARGET_ID } from "/src/constant/target_id.js";
+import { loadComponent } from "/src/helper/content_loader_helper.js";
 import { apiRequest } from "./api_request.js";
 import config from "../../config.js";
 
@@ -20,12 +24,23 @@ export async function getData(endpoint) {
  */
 export async function postData(endpoint, data, refresh_token) {
   const url = `${config.BASE_URL}${endpoint}`;
-  
-  if (refresh_token && refresh_token.refresh_token) {
-    return await apiRequest(url, "POST", data, {
-      Authorization: `Bearer ${refresh_token.refresh_token}`,
-    });
-  } else {
-    return await apiRequest(url, "POST", data);
+
+  try {
+    if (refresh_token && refresh_token.refresh_token) {
+      return await apiRequest(url, "POST", data, {
+        Authorization: `Bearer ${refresh_token.refresh_token}`,
+      });
+    } else {
+      return await apiRequest(url, "POST", data);
+    }
+  } catch (error) {
+    if (error.status === 401) {
+      loadComponent({
+        componentName: COMPONENTS.TOKEN_EXPIRE,
+        basePath: BASEPATH.PAGES,
+        targetId: TARGET_ID.DATA_OUTPUT,
+      });
+    }
+    throw error;
   }
 }
