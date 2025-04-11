dd31772995a56e19154d954ec7b3c0f06c94865f
deepali <deepali.sirsath1998@gmail.com>
2025-04-11 16:23:21 +0530

    added tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // other config...
+    rules: {
+      // other rules...
+      'import/no-absolute-path': 'off', // If you want to keep absolute paths
+      // OR
+      'import/no-unresolved': 'off' // If you're using path aliases
+    }
+  }
