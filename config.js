be47394fe54f7f481d0daf062e7e2493e16575c0
deepali <deepali.sirsath1998@gmail.com>
2025-05-27 11:34:19 +0530

    added changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // This is the base URL for the API
 
-// dev - http://192.168.22.40:10101/plugin
+// dev - http://3.109.178.115:10101/plugin
 // uat - http://192.168.22.40:10103/plugin
 // aws - http://3.109.178.115:8006/plugin
 // beta - http://192.168.0.2:5050/plugin
@@ -8,7 +8,7 @@
 // local_aws -  http://3.109.178.115:8009/plugin
 
 const config = {
-  BASE_URL: "http://3.109.178.115:8009/plugin",
+  BASE_URL: "http://192.168.0.127:8000/plugin",
 };
 
 export default config;
